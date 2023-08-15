9d12249c3d39b098cc72b5e432dc39bcf66b7d5d
Reuben Chavez <rchavez0215@gmail.com>
2023-08-14 20:17:17 -0700

    Got two AR hands to work

--- CV_RB_2023/Assets/HandTracking.cs
+++ CV_RB_2023/Assets/HandTracking.cs
@@ -7,23 +7,20 @@ public class HandTracking : MonoBehaviour
     public UDPReceive udpReceive;
     public GameObject[] handPoints;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         string data = udpReceive.data;
+        
         data = data.Remove(0, 1);
         data = data.Remove(data.Length-1, 1);
+        if (data == "None") { return;} 
+    
         string[] points = data.Split(',');
-
- 
-
-        for (int i = 0; i < 21; i++) 
+        int num_points = 21;
+        if (points.Length != 64) { num_points = 42; }
+        
+        for (int i = 0; i < num_points; i++) 
         {
             float x = 5 -  float.Parse(points[i * 3])/100;
             float y = float.Parse(points[i * 3 + 1])/100;
--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from cvzone.HandTrackingModule import HandDetector
 if __name__ == '__main__':
     
     # Webcam
-    width, height = 1280, 720
+    width, height = 640, 360
     cap = cv2.VideoCapture(0)
     cap.set(3, width)
     cap.set(4, height)
 
     # Hand Detector
-    detector = HandDetector(maxHands=1, detectionCon=0.8)
+    detector = HandDetector(maxHands=2, detectionCon=0.8)
 
     # Communications
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,22 +28,29 @@ if __name__ == '__main__':
         data = []
         # land mark values (x,y,z) * 21
         if hands: 
-            # Get the first hand detected
-            hand = hands[0]
+            # Get the first hand_01 detected
+            hand_01 = hands[0]
+        
             
             # Get the landmark list
-            lmList = hand['lmList']
-            # print(lmlist)
+            lmList = hand_01['lmList']
             for lm in  lmList:
                 data.extend([lm[0], height - lm[1], lm[2]])
-            sock.sendto(str.encode(str(data)), serverAdressPort)
-                
-        # else:
             
-        #     msg = "No hands detected"
-        #     sock.sendto(str.encode(str(msg)), serverAdressPort)
+            if(len(hands) == 2):
+                # Get the landmark list
+                hand_02 = hands[1]
+                lmList = hand_02['lmList']
+                for lm in  lmList:
+                    data.extend([lm[0], height - lm[1], lm[2]])
+                    
+            sock.sendto(str.encode(str(data)), serverAdressPort)
+        else:
+            
+            msg = "None"
+            sock.sendto(str.encode(str(msg)), serverAdressPort)
         
         
-        cv2.imshow("Image",img)
+        cv2.imshow("Hand Detector",img)
         cv2.waitKey(1)
         
