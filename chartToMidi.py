eb6c60a05e9c5bf96f85b0e39775722e732c3857
Reuben Chavez <rchavez0215@gmail.com>
2023-08-21 23:22:51 -0700

    Added sfx

--- chartToMidi.py
+++ chartToMidi.py
@@ -1,62 +1,55 @@
 import os
 import argparse
 import librosa
+import librosa.display
 from librosa.onset import onset_strength
 from librosa.util import normalize
 from midiutil.MidiFile import MIDIFile
 
-# Map pitch names to MIDI note numbers
-PITCH_MAP = {'F': 65, 'G': 67, 'A': 69, 'B': 71}
-
-
 def audio_to_midi(audio_file):
-    # Load the audio file
     y, sr = librosa.load(audio_file)
 
     # Compute the onset strength envelope
     onset_env = onset_strength(y=y, sr=sr)
     onset_env = normalize(onset_env)  # Normalize the onset envelope
 
-    # Get pitch information
-    pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
-    pitches = pitches.T  # Transpose for processing
+    # Extract chroma features
+    chroma = librosa.feature.chroma_stft(y=y, sr=sr)
 
-    print(f'Pitches {pitches} Mangitudes {magnitudes}')
-    
+    # Estimate the tempo of the audio
+    tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
+
     # Create a MIDI file
-    midi = MIDIFile(1)  # 1 track
+    midi = MIDIFile(1, deinterleave=False)  # Set deinterleave to False
     track = 0
     channel = 0
     time = 0
-    tempo = 120  # Set tempo (BPM)
+    midi.addTempo(track, time, tempo)  # Set tempo to match audio
 
-    midi.addTempo(track, time, tempo)
-
-    # Loop through time frames and add note events for specified pitches
-    for frame, frame_pitches in enumerate(pitches):
+    # Loop through time frames and add note events for detected pitches
+    for frame, chroma_frame in enumerate(chroma.T):
         # Get the corresponding onset strength
         onset_strength_env = onset_env[frame]
-        for pitch, magnitude in zip(frame_pitches, magnitudes[frame]):
-            pitch_name = librosa.midi_to_note(int(round(pitch)))
-            if pitch_name in PITCH_MAP and onset_strength_env > 0.1:  # Adjust threshold as needed
-                midi_pitch = PITCH_MAP[pitch_name]
-                duration = 0.1  # Duration of the note
+        max_strength_index = chroma_frame.argmax()
+        volume = onset_strength_env * 100  # Adjust as needed
 
-                midi.addNote(track, channel, midi_pitch,
-                             time, duration, volume=80)
+        if volume > 20:  # Adjust threshold as needed
+            midi_pitch = max_strength_index + 60  # MIDI note number
+            duration = 0.1  # Duration of the note
+            midi.addNote(track, channel, midi_pitch,
+                         time, duration, volume=volume)
 
         time += librosa.frames_to_time(1, sr=sr)
 
     # Write the MIDI file
-    output_midi_file = os.path.splitext(audio_file)[0] + '_notes.mid'
+    output_midi_file = os.path.splitext(audio_file)[0] + '_transcription.mid'
     with open(output_midi_file, 'wb') as f:
         midi.writeFile(f)
 
 
-# Replace these with your file paths
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Convert mp3 to MIDI highlighting specific pitches.")
+        description="Convert audio to MIDI transcription highlighting specific pitches.")
 
     parser.add_argument(
         "input", help="Path to the audio file (mp3, wav, etc.)")
@@ -64,4 +57,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     audio_file = args.input
+
     audio_to_midi(audio_file)
