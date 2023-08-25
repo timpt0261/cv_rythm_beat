57d5a1d8d5ef26d9e2fca6604a6054cbebcae271
Orangechrisy <chris@till.com>
2023-08-25 16:06:24 -0700

    score bar i guess

---
+++ CV_RB_2023/Assets/Scripts/Song_Manager/scorebar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scorebar : MonoBehaviour
+{
+	public float score = 0;
+	public float maxScore = 0;
+	private Image scoreBar;
+	public static scorebar Instance;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreBar = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		score = (float)Score_Manager.comboScore;
+		maxScore = (float)Score_Manager.maxScore;
+		if (maxScore != 0)
+		{
+			scoreBar.fillAmount = score / maxScore;
+		}
+    }
+}
